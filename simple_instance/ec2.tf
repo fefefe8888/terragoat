99088791fb51a6b11dd3cd98076c472e64215e83
fefefe8888 <fefefe@gmail.com>
2021-10-22 16:26:33 +0800

    Update ec2.tf

--- simple_instance/ec2.tf
+++ simple_instance/ec2.tf
@@ -23,7 +23,6 @@ resource "aws_security_group" "ssh_traffic" {
     git_repo             = "terragoat"
     yor_trace            = "f428110a-489a-4b3d-8a5a-9b7f077ae2f7"
   }
-  vpc_id = "vpc-e073b39d"
 }
 
 resource "aws_instance" "web_server_instance" {
@@ -41,24 +40,7 @@ resource "aws_instance" "web_server_instance" {
     git_repo             = "terragoat"
     yor_trace            = "34ed6144-1015-40e5-ab5f-607b96b2768c"
   }
-  associate_public_ip_address = true
-  availability_zone           = "us-east-1d"
-  cpu_core_count              = "1"
-  cpu_threads_per_core        = "1"
-  credit_specification        = { "cpu_credits" : "standard" }
-  disable_api_termination     = false
-  ebs_optimized               = false
-  get_password_data           = false
-  hibernation                 = false
-  ipv6_address_count          = "0"
-  metadata_options            = { "http_endpoint" : "enabled", "http_put_response_hop_limit" : "1", "http_tokens" : "optional" }
-  monitoring                  = false
-  private_ip                  = "172.31.87.72"
-  root_block_device           = { "delete_on_termination" : true, "encrypted" : false, "iops" : "100", "volume_size" : "8", "volume_type" : "gp2" }
-  source_dest_check           = true
-  subnet_id                   = "subnet-fe8b39df"
-  tenancy                     = "default"
-  vpc_security_group_ids      = ["sg-0cba3978f047b2c64"]
+
 }
 
 data "aws_ami" "ubuntu" {
