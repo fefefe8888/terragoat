78770b4d55d404031a9f0bc1d2299f6734012975
fefefe8888 <fefefe@gmail.com>
2021-10-22 19:23:07 +0800

    Update ec2.tf

--- simple_instance/ec2.tf
+++ simple_instance/ec2.tf
@@ -58,3 +58,13 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+}
