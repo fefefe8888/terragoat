3fa1c00664333c738673701068e1faff750a4c80
fefefe8888 <fefefe@gmail.com>
2021-10-22 17:28:01 +0800

    Update ec2.tf

--- simple_instance/ec2.tf
+++ simple_instance/ec2.tf
@@ -13,33 +13,13 @@ resource "aws_security_group" "ssh_traffic" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    git_commit           = "001a8238cf1d5e370c929d2f97aa010c081cdc34"
-    git_file             = "simple_instance/ec2.tf"
-    git_last_modified_at = "2021-10-22 05:31:06"
-    git_last_modified_by = "fefefe@gmail.com"
-    git_modifiers        = "fefefe"
-    git_org              = "fefefe8888"
-    git_repo             = "terragoat"
-    yor_trace            = "f428110a-489a-4b3d-8a5a-9b7f077ae2f7"
-  }
 }
 
 resource "aws_instance" "web_server_instance" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ssh_traffic.name}"]
-  tags = {
-    Name                 = "bc_workshop_ec2"
-    git_commit           = "001a8238cf1d5e370c929d2f97aa010c081cdc34"
-    git_file             = "simple_instance/ec2.tf"
-    git_last_modified_at = "2021-10-22 05:31:06"
-    git_last_modified_by = "fefefe@gmail.com"
-    git_modifiers        = "fefefe"
-    git_org              = "fefefe8888"
-    git_repo             = "terragoat"
-    yor_trace            = "34ed6144-1015-40e5-ab5f-607b96b2768c"
-  }
+
 
 }
 
